fde01e293401c4995ba816d2c4fdc1391ef80939
Prasanna <prasannatm001@gmail.com>
2020-12-24 14:08:42 +0530

    first

--- CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/InterviewSchedulerControllerTest.java
+++ CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/InterviewSchedulerControllerTest.java
@@ -9,15 +9,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
 import com.spring.cg.json.Candidate;
+import com.spring.cg.json.InterviewScheduler;
 
 class InterviewSchedulerControllerTest {
 
 	@Test
+	public void giveTechRating() {
+		RestTemplate restTemplate = new RestTemplate();
+		InterviewScheduler interviewScheduler=restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/30001002",InterviewScheduler.class);
+		assertNotNull(interviewScheduler);
+
+	}
+	
+	
+	
+	
+	@Test
 	void viewInterviewMembers() {
 		RestTemplate restTemplate = new RestTemplate();
-		Candidate candidate = restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/viewallmembers", Candidate.class);
+		List<Candidate> candidate = (List<Candidate>) restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/viewallmembers", Candidate.class);
 		assertNotNull(candidate);
 		
 	}
+	
 
 }
