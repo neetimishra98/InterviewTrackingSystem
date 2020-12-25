18c7708e96f9ada015388215c9504968a8acdd74
Prasanna <prasannatm001@gmail.com>
2020-12-25 23:10:55 +0530

    test cases

--- CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/InterviewSchedulerControllerTest.java
+++ CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/InterviewSchedulerControllerTest.java
@@ -2,35 +2,79 @@ package com.spring.cg.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.spring.cg.json.Candidate;
 import com.spring.cg.json.InterviewScheduler;
 
 class InterviewSchedulerControllerTest {
+	
+	
+	
+	//TEST CASE TO VIEW INTERVIEW MEMBERS BY GIVING CORRECT INTERVIEW ID - PASS
+		@Test
+		void viewInterviewMembersForTech() {
+			RestTemplate restTemplate = new RestTemplate();
+			Candidate candidate = restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/viewinterviewmembersfortech/30001004", Candidate.class);
+			assertNotNull(candidate);
 
-	@Test
-	public void giveTechRating() {
-		RestTemplate restTemplate = new RestTemplate();
-		InterviewScheduler interviewScheduler=restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/30001002",InterviewScheduler.class);
-		assertNotNull(interviewScheduler);
-
-	}
-	
-	
-	
-	
-	@Test
-	void viewInterviewMembers() {
-		RestTemplate restTemplate = new RestTemplate();
-		List<Candidate> candidate = (List<Candidate>) restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/viewallmembers", Candidate.class);
-		assertNotNull(candidate);
+			/*
+			InterviewScheduler interviewScheduler=restTemplate.getForObject
+			("http://localhost:8080/cgits/interviewscheduler/tech/viewinterviewmembersfortech/30001004", InterviewScheduler.class);
+			Candidate candidate=interviewScheduler.getCandidate();
+			*/
+			
+		}
 		
-	}
-	
+		
+		//TEST CASE TO CHECK TECHRATING BY GIVING CORRECT INTERVIEW ID - PASS
+		@Test
+		public void giveTechRating() {
+			RestTemplate restTemplate = new RestTemplate();
+			InterviewScheduler interviewScheduler=restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/30001008",InterviewScheduler.class);
+			assertNotNull(interviewScheduler.getTechrating());	
 
+		}
+		
+	/*	
+		
+		//TEST CASE TO VIEW INTERVIEW MEMBERS BY GIVING CANDIDATE ID - FAIL
+		@Test
+		void viewInterviewMembersForTechUsingCandidateId() {
+			RestTemplate restTemplate = new RestTemplate();
+			Candidate candidate = restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/viewinterviewmembersfortech/20001004", Candidate.class);
+			assertNotNull(candidate);
+		}
+		
+	
+	//TEST CASE TO CHECK TECHRATING BY GIVING INTERVIEW ID AS ALL 0s - FAIL
+		@Test
+		public void giveTechRatingForZeros() {
+			RestTemplate restTemplate = new RestTemplate();
+			InterviewScheduler interviewScheduler=restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/00000000",InterviewScheduler.class);
+			assertNotNull(interviewScheduler.getTechrating());	
+
+		}	
+		
+		//TEST CASE TO CHECK TECHRATING BY GIVING INTERVIEW ID AS A NEGATIVE NUMBER - FAIL
+		@Test
+		public void giveTechRatingForNegative() {
+			RestTemplate restTemplate = new RestTemplate();
+			InterviewScheduler interviewScheduler=restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/-30001008",InterviewScheduler.class);
+			assertNotNull(interviewScheduler.getTechrating());	
+		}
+		
+		//TEST CASE TO CHECK TECHRATING BY GIVING INTERVIEW ID(SIZE)- FAIL
+				@Test
+				public void giveTechRatingForSize() {
+					RestTemplate restTemplate = new RestTemplate();
+					InterviewScheduler interviewScheduler=restTemplate.getForObject("http://localhost:8080/cgits/interviewscheduler/tech/3005555550001008",InterviewScheduler.class);
+					assertNotNull(interviewScheduler.getTechrating());	
+				}
+		*/
 }
