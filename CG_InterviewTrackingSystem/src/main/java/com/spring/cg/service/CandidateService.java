356b2be9356e36fa91b9e95e46b023a79d7e00ab
Sneha Chitale <snehachitale26@gmail.com>
2020-12-27 14:53:09 +0530

    view candidate updated

--- CG_InterviewTrackingSystem/src/main/java/com/spring/cg/service/CandidateService.java
+++ CG_InterviewTrackingSystem/src/main/java/com/spring/cg/service/CandidateService.java
@@ -11,10 +11,9 @@ public interface CandidateService {
 	public Candidate createCandidate(Candidate candidate);
 	public List<Candidate> getAllCandidates();
 	public Candidate getCandidateById(int candidateid) throws CandidateNotFoundException;
-	public Candidate getCandidateByIdForHR(int candidateid) throws CandidateNotFoundException;
 	
-	//To view Candidate For HR
-	public Map<Candidate, String> listInterviewCandidates(int candidateid) throws CandidateNotFoundException;
+	//To view Candidate For HR USING CANDIDATE ID
+	public Map<Candidate, String> viewCandidateById(int candidateid) throws CandidateNotFoundException;
 	
 	//To view Candidate For Tech
 	public Map<Candidate, String> listTechInterviewCandidates(int candidateid) throws CandidateNotFoundException;
