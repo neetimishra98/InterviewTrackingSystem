3485a9c469e827f385d4f18a0ac4dfc2b0c71553
Suraj <surajmondal888@gmail.com>
2020-12-27 16:05:19 +0530

    Updated Update and cancel interview schedule

--- CG_InterviewTrackingSystem/src/main/java/com/spring/cg/exception/GlobalExceptionHandler.java
+++ CG_InterviewTrackingSystem/src/main/java/com/spring/cg/exception/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	
@@ -49,6 +50,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST,ex.getLocalizedMessage(),error);
 		return new ResponseEntity<ErrorMessage>(errorMessage,new HttpHeaders(), errorMessage.getStatus());
 	}
+	
+	@ExceptionHandler(value = {InterviewSchedulerNotFoundException.class})
+	public ResponseEntity<ErrorMessage> handleInterviewSchedulerNotFoundException(
+			CandidateNotFoundException ex) {
+		String error = "Interview not found";
+
+	ErrorMessage errorMessage = 
+      new ErrorMessage(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error);
+    return new ResponseEntity<ErrorMessage>(
+    		errorMessage, new HttpHeaders(), errorMessage.getStatus());
+	
+}
 	
 	@Override
 	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
