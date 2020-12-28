949b9a7694331928ac6ffa1b601d947226581719
Sneha Chitale <snehachitale26@gmail.com>
2020-12-28 17:50:30 +0530

    test updated

--- CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/CandidateControllerTest.java
+++ CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/CandidateControllerTest.java
@@ -245,81 +245,78 @@ public class CandidateControllerTest {
 			
 
 	//TEST CASE TO VIEW A CANDIDATE FOR HR BY GIVING CORRECT PANEL ID - PASS
-	@Test
-	public void testFindCandidateById() {
-		logger.info("[START] testFindCandidateById()");
-		RestTemplate restTemplate = new RestTemplate();
-		Candidate candidate=restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/12345678", Candidate.class);
-		assertNotNull(candidate);
-		logger.info("[END] testFindCandidateById()");
-	}
+			@Test
+			public void testCandidateById() {
+				logger.info("[START] testCandidateById()");
+				RestTemplate restTemplate = new RestTemplate();
+				Candidate candidate=restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/12345678", Candidate.class);
+				assertNotNull(candidate);
+				logger.info("[END] testCandidateById()");
+			}
+				
+			//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE ID - PASS
+			@Test
+			public void testFindCandidateByIdHr() {
+				logger.info("[START] testFindCandidateByIdHr()");
+				RestTemplate restTemplate = new RestTemplate();
+				ResponseEntity<Candidate> responseEntity = restTemplate.getForEntity("http://localhost:9091/cgits/candidate/hr/12345678", Candidate.class);
 			
-	//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE ID - PASS
-	@Test
-	public void testViewCandidateByIdHr() {
-		logger.info("[START] testViewCandidateByIdHr()");
-		RestTemplate restTemplate = new RestTemplate();
-		Candidate candidate=restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/12345678", Candidate.class);
-		assertEquals(candidate.getCandidateid(), 12345678);
-		logger.info("[END] testViewCandidateByIdHr()");
-	}
-	
-	//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE ID - Fail
-	@Test
-	public void testViewCandidateByIdForHRFail_CandidateNotFoundException(){
+				assertNotNull(responseEntity);
+				logger.info("[END] testFindCandidateByIdHr()");
+			}
 			
-		logger.info("[START] testViewCandidateByIdForHrFail()");
-		RestTemplate restTemplate = new RestTemplate();
-			
-		assertThrows(CandidateNotFoundException.class,
-		()->{
-			candidateService.viewCandidateById(-2);
-		}	
-	);
-		logger.info("[END] testViewCandidateByIdForHrFail()");
-	}
-	
-	//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE NAME - PASS
-	@Test
-	public void testViewCandidateByNameHr() {
-		logger.info("[START] testViewCandidateByNameHr()");
-		RestTemplate restTemplate = new RestTemplate();
-		Candidate candidate=restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/sean", Candidate.class);
-		assertEquals(candidate.getCandidateid(), "sean");
-		logger.info("[END] testViewCandidateByNameHr()");
-	}
+			//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE ID - Fail
+			@Test
+			public void testViewCandidateByIdForHRFail_CandidateNotFoundException(){
+				    logger.info("[START] testViewCandidateByIdForHRFail()");		
+					RestTemplate restTemplate = new RestTemplate();
+					assertThrows(CandidateNotFoundException.class,
+					()->{
+							candidateService.viewCandidateForHR("-1111");
+					}	
+				);
+					logger.info("[END] testViewCandidateByIdForHRFail()");
+			}
 
-	//TEST CASE TO VIEW A CANDIDATE FOR HR USING INVALID CANDIDATE NAME - FAIL
-	@Test
-	public void testViewCandidateByNameInvalid() {
-		logger.info("[START] testViewCandidateByNameInvalid()");
-		RestTemplate restTemplate = new RestTemplate();
-		try {
-			candidateInvalid = restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/tom", Candidate.class);
+			//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE NAME - PASS
+			@Test
+			public void testFindCandidateByName() {
+				logger.info("[START] testFindCandidateByName()");
+				RestTemplate restTemplate = new RestTemplate();
+				ResponseEntity<Candidate> responseEntity = restTemplate.getForEntity("http://localhost:9091/cgits/candidate/hr/sean", Candidate.class);
+					
+				assertNotNull(responseEntity);
+				logger.info("[END] testFindCandidateByName()");
+			}
+
+			//TEST CASE TO VIEW A CANDIDATE FOR HR USING CANDIDATE NAME - FAIL
+			@Test
+			public void testViewCandidateByNameHRFail(){	
+				logger.info("[START] testViewCandidateByNameHRFail()");
+				RestTemplate restTemplate = new RestTemplate();	
+				assertThrows(CandidateNotFoundException.class,
+				()->{
+						candidateService.viewCandidateForHR("Neha");
+					}	
+				);		
+				logger.info("[END] testViewCandidateByNameHRFail()");
+			}
+				
+				
+		//TEST CASE TO VIEW A CANDIDATE FOR HR USING <<BLANK>> PATH VARIABLE - FAIL
+		@Test
+		public void testViewCandidateByBlank() {
+			logger.info("[START] testViewCandidateByNameBlank()");
+			RestTemplate restTemplate = new RestTemplate();
+			try {
+				candidateInvalid = restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/", Candidate.class);
+			}
+			catch(Exception e){
+				candidateInvalid = null;
+			}
+			finally{
+				assertNotNull(candidateInvalid, "<<BLANK>> Was Not Found");
+			}
+			logger.info("[START] testViewCandidateByBlank()");
 		}
-		catch(Exception e){
-			candidateInvalid = null;
-		}
-		finally{
-			assertNotNull(candidateInvalid, "Tom Was Not Found");
-		}
-		logger.info("[START] testViewCandidateByNameInvalid()");
-	}
-			
-	//TEST CASE TO VIEW A CANDIDATE FOR HR USING <<BLANK>> PATH VARIABLE - FAIL
-	@Test
-	public void testViewCandidateByBlank() {
-		logger.info("[START] testViewCandidateByNameBlank()");
-		RestTemplate restTemplate = new RestTemplate();
-		try {
-			candidateInvalid = restTemplate.getForObject("http://localhost:9091/cgits/candidate/hr/", Candidate.class);
-		}
-		catch(Exception e){
-			candidateInvalid = null;
-		}
-		finally{
-			assertNotNull(candidateInvalid, "<<BLANK>> Was Not Found");
-		}
-		logger.info("[START] testViewCandidateByBlank()");
-	}
 }
--- CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/PanelMemberControllerTest.java
+++ CG_InterviewTrackingSystem/src/test/java/com/spring/cg/controller/PanelMemberControllerTest.java
@@ -3,36 +3,66 @@ package com.spring.cg.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
+import com.spring.cg.exception.PanelMemberNotFoundException;
 import com.spring.cg.json.PanelMember;
+import com.spring.cg.service.PanelMemberService;
 
 class PanelMemberControllerTest {
 
 	public PanelMember panelMemberInvalid;
 	
-	//TEST CASE TO FIND PANEL MEMBER BY GIVING CORRECT PANEL ID - PASS
-	@Test
-	public void testFindPanelMemberById() {
-		RestTemplate restTemplate = new RestTemplate();
-		PanelMember panelMember = restTemplate.getForObject("http://localhost:9091/cgits/panelmember/hr/11000000", PanelMember.class);
-		assertEquals(panelMember.getPanelid(), 11000000);
-	}
+	@Autowired
+	private PanelMemberService panelMemberService;
 	
-	//TEST CASE TO FIND PANEL MEMBER BY GIVING BLANK PANEL ID - FAIL
-	@Test
-	public void testFindPanelMemberByBlank() {
-		RestTemplate restTemplate = new RestTemplate();
-		try {
-			panelMemberInvalid = restTemplate.getForObject("http://localhost:9091/cgits/panelmember/hr/", PanelMember.class);
+	//TEST CASE TO FIND PANEL MEMBER BY GIVING CORRECT PANEL ID - PASS
+		@Test
+		public void testFindPanelMemberById() {
+			RestTemplate restTemplate = new RestTemplate();
+			PanelMember panelMember = restTemplate.getForObject("http://localhost:9091/cgits/panelmember/hr/11000000", PanelMember.class);
+			assertEquals(panelMember.getPanelid(), 11000000);
 		}
-		catch(Exception e){
-			panelMemberInvalid = null;
+		
+		//TEST CASE TO FIND PANEL MEMBER BY GIVING BLANK PANEL ID - FAIL
+		@Test
+		public void testFindPanelMemberByBlank() {
+			RestTemplate restTemplate = new RestTemplate();
+			try {
+				panelMemberInvalid = restTemplate.getForObject("http://localhost:9091/cgits/panelmember/hr/", PanelMember.class);
+			}
+			catch(Exception e){
+				panelMemberInvalid = null;
+			}
+			finally{
+				assertNotNull(panelMemberInvalid, "<<BLANK>> Was Not Found");
+			}
 		}
-		finally{
-			assertNotNull(panelMemberInvalid, "<<BLANK>> Was Not Found");
+		
+		//TEST CASE TO VIEW A CANDIDATE FOR HR USING NEGATIVE PANEL ID - Fail
+		@Test
+		public void testFindPanelMemberByIdForHRFail_PanelMemberNotFoundException(){
+								
+			RestTemplate restTemplate = new RestTemplate();						
+			assertThrows(PanelMemberNotFoundException.class,
+			()->{
+				panelMemberService.surrenderAsHRPanel(-1111);
+				}	
+			);
 		}
-	}
+		
+		//TEST CASE TO VIEW A CANDIDATE FOR HR USING WRONG PANEL ID SIZE - FAIL
+		@Test
+		public void testFindPanelMemberByIdForHRSize_PanelMemberNotFoundException(){
+									
+			RestTemplate restTemplate = new RestTemplate();						
+			assertThrows(PanelMemberNotFoundException.class,
+			()->{
+					panelMemberService.surrenderAsHRPanel(1111);
+				}	
+			);
+		}
 	
 	//ADD PANEL MEMBER TEST CASES
 	//PANEL MEMBER WITH VALID EMPLOYEE FOREIGN KEY
